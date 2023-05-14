8ec68a2d8dfbfded194faa852f14bc5ff4b233b1
Harsh Tripathi <103751175+TripathiHARSHji@users.noreply.github.com>
2023-05-14 21:29:42 +0530

    Add files via upload

--- src/main/java/com/searchmf/repository/MutualFundsRepository.java
+++ src/main/java/com/searchmf/repository/MutualFundsRepository.java
@@ -11,10 +11,10 @@ public interface MutualFundsRepository extends JpaRepository<MutualFunds, Long> 
 	
 
 		
-	   @Query(value ="SELECT * FROM Mutual_funds  WHERE schema_name LIKE CONCAT('%', :keyword, '%') OR schema_category LIKE CONCAT('%', :keyword, '%') OR fund_house LIKE CONCAT('%', :keyword, '%')  limit 5",nativeQuery = true )
+	   @Query(value ="SELECT * FROM mutual_funds  WHERE schema_name LIKE CONCAT('%', :keyword, '%') OR schema_category LIKE CONCAT('%', :keyword, '%') OR fund_house LIKE CONCAT('%', :keyword, '%')  limit 5",nativeQuery = true )
 	    List<MutualFunds> searchMutualFunds( String keyword);
 	   
-	   @Query(value="SELECT * FROM Mutual_funds WHERE schema_id = :id",nativeQuery = true )
+	   @Query(value="SELECT * FROM mutual_funds WHERE schema_id = :id",nativeQuery = true )
 	   List<MutualFunds> searchMutualFundsById(String id);
 	   
 }
